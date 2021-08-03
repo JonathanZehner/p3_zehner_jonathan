8e7d4e8c1b62576b6f396b221dd6640dac20e0cd
Jonathan A. Zehner <78240659+JonathanZehner@users.noreply.github.com>
2021-08-03 15:28:21 -0400

    styled hamburger menu and added java code to main.js;

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+// https://codepen.io/g13nn/pen/eHGEF
 
+
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+  $( ".hamburger" ).hide();
+  $( ".cross" ).show();
+$( ".menu" ).slideToggle( "slow", function() {
+});
+});
+
+$( ".cross" ).click(function() {
+  $( ".cross" ).hide();
+  $( ".hamburger" ).show();
+$( ".menu" ).slideToggle( "slow", function() {
+});
+});
+
+});
