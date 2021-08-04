b689f2f125d9c36ee3cae45a93e5bb7f968c64d6
Jonathan A. Zehner <78240659+JonathanZehner@users.noreply.github.com>
2021-08-03 20:42:02 -0400

    added 500 min-width media query; incorporated accordion menu on article;

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+//Hamburger Menu
 // https://codepen.io/g13nn/pen/eHGEF
-
 
 $( document ).ready(function() {
 
@@ -20,3 +20,26 @@ $( ".menu" ).slideToggle( "slow", function() {
 });
 
 });
+
+
+// Accordion menu
+// https://www.w3schools.com/howto/howto_js_accordion.asp
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
